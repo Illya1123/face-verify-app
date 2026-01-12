4ba7225759f1318144655c39abe101ec64e6ecb9
Lê Quốc Anh <lequocanh0101@gmail.com>
2026-01-12 17:36:26 +0700

    feat: config github workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'face-verify-app'
+
 const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: !isProd,
 })
 
 module.exports = withPWA({
     reactStrictMode: true,
+
+    output: 'export',
+
+    basePath: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `/${repoName}/` : '',
+
+    images: {
+        unoptimized: true,
+    },
 })
